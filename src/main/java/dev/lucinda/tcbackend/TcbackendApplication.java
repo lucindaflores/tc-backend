26a980e77b7c62e2188848eb1296cc90494f2140
Lucinda Flores <lucindafloresbriones@gmail.com>
2026-09-17 14:01:20 +0200

    Project initialization

---
+++ src/main/java/dev/lucinda/tcbackend/TcbackendApplication.java
@@ -0,0 +1,13 @@
+package dev.lucinda.tcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lucinda/tcbackend/TcbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucinda.tcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
